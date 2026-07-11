787550cfe8a16a29ad2ae437a486275cabcee977
Poorvi230 <poorvigarg12.09@gmail.com>
2026-07-11 23:59:54 +0530

    canvas loop n testing player shapes

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const centerX = canvas.innerWidth / 2;
-const centerY = canvas.innerheight / 2;
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
 
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
@@ -25,18 +25,15 @@ class Player {
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.fillStyle = this.color;
         ctx.fill();
-
-        ctx.lineWidth = 4;
-        ctx.strokeStyle = '#c5832b'
-        ctx.stroke();
     }
 }
-const player = new Player(centerX, centerY, 20, 'white');
+const player = new Player(centerX, centerY, 25, '#005B8F');
 
 function animate() {
 requestAnimationFrame(animate);
 
-ctx.fillStyle = 'rgba(5, 5, 16, 1)';
-ctx.fillRect(0, 0, canvas.width, canvas,height);
+ctx.fillStyle = '#f4f4f0';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 player.draw();
 }
+animate();
